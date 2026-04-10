af6100b659fd5d3a1df4da3884d848513285e53a
Robert Smallshire <robert@smallshire.org.uk>
2026-04-10 16:57:16 +0200

    Bump version: 2.0.1 → 3.0.0

--- src/oaknut_dfs/__init__.py
+++ src/oaknut_dfs/__init__.py
@@ -34,7 +34,7 @@ from oaknut_dfs.formats import (
 
 Version = namedtuple("Version", ["major", "minor", "patch"])
 
-__version__ = "2.0.1"
+__version__ = "3.0.0"
 __version_info__ = Version(*(__version__.split(".")))
 
 __all__ = [
